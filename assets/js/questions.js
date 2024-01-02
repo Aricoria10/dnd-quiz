5985196a212b0f2ecb025c39163d0b932d1540fc
Aricoria10 <empresskitsune10@gmail.com>
2024-01-01 20:12:13 -0500

    adding inital text to html and starter code for questions js

---
+++ assets/js/questions.js
@@ -0,0 +1,20 @@
+let questionIndex = 0;
+
+const questions = [
+    {
+        q:"How do we create an object?",
+        a:["Square Brackets","Double Quotes","Curly Braces"],
+        c:"Curly Braces"
+    },
+    {
+
+    }
+]
+// Current Question is
+questions[questionIndex].q
+// Current answers 
+for (let index = 0; index < questions[questionIndex].a.length; index++) {
+    // be each answer
+    const element = questions[questionIndex].a[index];
+    
+}
