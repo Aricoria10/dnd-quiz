6ed81abad29238c439514369c09e8be6bf264cac
Aricoria10 <empresskitsune10@gmail.com>
2024-01-02 20:19:22 -0500

    finished adding questions and set up buttons

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,3 +1,4 @@
+var startbtn = document.querySelector("#startbtn");
 let questionIndex = 0;
 
 const questions = [
@@ -12,7 +13,34 @@ const questions = [
         c:"Toril"
     },
     {
-        q: ""
+        q:"Which of the options below is not a class offered in the fifth edition Player's Handbook?",
+        a:["Warlock", "Barbarian", "Warrior", "Monk"],
+        c:"Warrior"
+    },
+    {
+        q:"Which of these is not one of the elven subrace?",
+        a:["Ar-Tel-Quessir", "Ur-Tel-Quessir", "Teu-Tel-Quessir", "Ssri-Tel-Quessir"],
+        c:"Ur-Tel-Quessir"
+    },
+    {
+        q:"Which of these is not a method that has been used to determine if a character hits or misses with an attack?",
+        a:["Accuracy Rating", "THAC0", "Base attack Bonus", "Proficiency Bonus"],
+        c:"Accuracy Rating"
+    },
+    {
+        q:"How many schools of Magic are there?",
+        a:["4", "6", "8", "10"],
+        c:"8"
+    },
+    {
+        q:"During the times of trouble, which god was not exiled to the material plane?",
+        a:["Tyr", "Bane", "Helm", "Lathandar"],
+        c:"Helm"   
+    },
+    {
+        q:"The Mulhorandi are a group of humans that were taken from earth. What culture of the ancienty world were they taken from?",
+        a:["Ancient Romans", "Ancient Chinese", "Ancient Persians", "Ancient Egyptians"],
+        c:"Ancient Egyptians"
     }
 ]
 // Current Question is
@@ -23,3 +51,7 @@ for (let index = 0; index < questions[questionIndex].a.length; index++) {
     const element = questions[questionIndex].a[index];
     
 }
+
+startbtn.addEventListener("push", function() {
+    
+})
